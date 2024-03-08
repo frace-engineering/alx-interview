2ab8cef4dff2c61c3897ebcd56077a1f8f13e69a
Engr Ugwu <fugwungene@gmail.com>
2024-03-08 11:36:50 +0000

    Add extra blank line @ line 4.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a function that implements pascal triangle """
 
+
 def pascal_triangle(n):
     """ Test the value of n and return empty list if n <= 0"""
     if n <= 0:
