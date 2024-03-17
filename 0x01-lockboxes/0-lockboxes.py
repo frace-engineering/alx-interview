e2b2429f2626a28b20d7f5d44b77e928fd52d491
Engr Ugwu <fugwungene@gmail.com>
2024-03-17 01:08:43 +0000

    Add documentation to code.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,15 @@
 from collections import deque
 
-
+"""Define afunction named canUnlockAll that determines
+if given boxes can be unlocked
+"""
 def canUnlockAll(boxes):
     n = len(boxes)
     visited = [False] * n
     visited[0] = True
     queue = deque([0])
 
+    """Iterate through the Items in the boxes to check foe correct key"""
     while queue:
         current_box = queue.popleft()
         for key in boxes[current_box]:
