8f8fcaef416dfb34f8b033e4ca3fe19d09b91d9d
Engr Ugwu <fugwungene@gmail.com>
2024-03-22 21:51:29 +0000

    Add project task
    Add a readme file.
    Add task 0.

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Calculate minimum operations to achieve a result."""
+
+
+def minOperations(n):
+    """
+    In a text file, there is a single character H. Your text editor can
+    execute only two operations in this file: Copy All and Paste. Given
+    a number n, write a method that calculates the fewest number of
+    operations needed to result in exactly nH characters in the file.
+
+    Args:
+        n of type integer.
+
+    Return:
+        Minimum number of operations
+    """
+    if n == 1:
+        return 1
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,16 @@
+Readme file for the Minimum Operations project.
+
+1. 0-minoperations.py is a  file that contains the code that achieves the bellow task.
+
+In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+
+Number of operations: 6
