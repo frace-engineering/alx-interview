06fc7eecfd3dabc2aa217189389f77b03d86e70b
Engr Ugwu <fugwungene@gmail.com>
2024-03-28 13:22:57 +0000

    Fix pycodestyle issue.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,11 +11,13 @@ def print_statistics(lines_count, total_file_size, status_code_counts):
         if status_code_counts[status_code] > 0:
             print(f"{status_code}: {status_code_counts[status_code]}")
 
+
 def main():
     """Entry point of the function"""
     lines_count = 0
     total_file_size = 0
-    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0,
+                          403: 0, 404: 0, 405: 0, 500: 0}
 
     try:
         """Read stdin line by line"""
@@ -31,10 +33,12 @@ def main():
             lines_count += 1
 
             if lines_count % 10 == 0:
-                print_statistics(lines_count, total_file_size, status_code_counts)
+                print_statistics(lines_count, total_file_size,
+                                 status_code_counts)
 
     except KeyboardInterrupt:
         print_statistics(lines_count, total_file_size, status_code_counts)
+
 
 if __name__ == "__main__":
     main()
