7f8e2f9fcd84fab395cdad424d98157398a430f7
Engr Ugwu <fugwungene@gmail.com>
2024-04-17 00:21:59 +0000

    Add task file.

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+function getMovieCharacters(movieId) {
+    const url = `https://swapi.dev/api/films/${movieId}/`;
+    return axios.get(url)
+        .then(response => {
+            const charactersUrls = response.data.characters;
+            return Promise.all(charactersUrls.map(characterUrl =>
+                axios.get(characterUrl)
+                    .then(characterResponse => characterResponse.data.name)
+                    .catch(error => {
+                        console.error(`Failed to retrieve character data for URL: ${characterUrl}`);
+                        return null;
+                    })
+            ));
+        })
+        .catch(error => {
+            console.error(`Failed to retrieve movie data for ID: ${movieId}`);
+            return null;
+        });
+}
+
+function main() {
+    const args = process.argv.slice(2);
+    if (args.length !== 1) {
+        console.error("Usage: node script.js <Movie ID>");
+        process.exit(1);
+    }
+
+    const movieId = args[0];
+    getMovieCharacters(movieId)
+        .then(characters => {
+            if (characters) {
+                characters.forEach(character => console.log(character));
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+main();
