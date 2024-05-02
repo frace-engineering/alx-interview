715a7218f27ff4aa6ab0108a547631bad8d086d1
Engr Ugwu <fugwungene@gmail.com>
2024-05-02 22:09:43 +0000

    Modify code to improve efficiency.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,8 +8,23 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    count = 0
+    coins.sort(reverse=True)
+    for i in range(len(coins)):
+        if coins[i] <= total:
+            count += total // coins[i]
+            total = total % coins[i]
+            if total != 0 and total < coins[len(coins) - 1]:
+                count = -1
+            makeChange(coins, total)
+    return count
+
+
+
+    """
     dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[-1] if dp[-1] != float("inf") else -1
+    """
